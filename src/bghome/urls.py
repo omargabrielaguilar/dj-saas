febd585b2c44d3c1bf5f1de161dcc995f5b6f663
omar <saucyfunnel@gmail.com>
2024-12-01 16:22:05 -0500

    set up views

--- src/bghome/urls.py
+++ src/bghome/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 
 from .views import home_page_view
 urlpatterns = [
-    path('/', home_page_view), #root
+    path('', home_page_view), #root
     path('admin/', admin.site.urls),
 ]
--- src/bghome/views.py
+++ src/bghome/views.py
@@ -1,5 +1,27 @@
+import pathlib
+
+from django.shortcuts import render
 from django.http import HttpResponse
 
+this_dir = pathlib.Path(__file__).resolve().parent
 
 def home_page_view(request, *args, **kwargs):
-  return HttpResponse("<h1>Hola</h1>")
+  print(this_dir)
+  
+  title = "page"
+  context = {
+    "page_title" : title
+  }
+  
+  html_template  = "home.html"
+  return render(request, html_template)  
+
+
+def old_home_page_view(request, *args, **kwargs):
+  print(this_dir)
+  
+  html_ = ""
+  html_file_path = this_dir / "home.html"
+  html_ = html_file_path.read_text()  #important read_text method <>
+  
+  return HttpResponse(html_)
